6bf2762189e2d3f55894cb7bc6982d8bd4894f00
Toby Jorris <tobyjorris@gmail.com>
2020-03-07 09:07:55 -0600

    Added Radium

--- 03 Styling React Components/src/App.js
+++ 03 Styling React Components/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Radium, {StyleRoot} from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -49,7 +50,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -69,6 +74,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+            color: 'black'
+      };
     }
 
     const classes = [];
@@ -80,6 +89,7 @@ class App extends Component {
     }
 
     return (
+    <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
@@ -88,8 +98,9 @@ class App extends Component {
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
-    );
+    </StyleRoot>
+      );
   }
 }
 
-export default App;
+export default Radium(App);

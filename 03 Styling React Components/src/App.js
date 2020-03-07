544618107d648fd8b8aafb29eeeafe58a73bab5b
Toby Jorris <tobyjorris@gmail.com>
2020-03-07 09:19:11 -0600

    Removed Radium, added Styled Components

--- 03 Styling React Components/src/App.js
+++ 03 Styling React Components/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Radium, {StyleRoot} from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -89,7 +88,7 @@ class App extends Component {
     }
 
     return (
-    <StyleRoot>
+
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
@@ -98,9 +97,8 @@ class App extends Component {
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
-    </StyleRoot>
       );
   }
 }
 
-export default Radium(App);
+export default App;

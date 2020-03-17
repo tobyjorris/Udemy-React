9f7654be5ce598a2c09187f0b11fee905093c334
Toby Jorris <tobyjorris@gmail.com>
2020-03-16 19:15:28 -0500

    Updated Toggle button to use Styled Components

--- 03 Styling React Components/src/App.js
+++ 03 Styling React Components/src/App.js
@@ -1,21 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import styled from 'styled-components';
 import Person from './Person/Person';
 
-const StyledButton = styled.button`
-      background-color: ${props => props.alt ? 'red' : 'green'};
-      color: white;
-      font: inherit;
-      border: 1px solid blue;
-      padding: 8px;
-      cursor: pointer;
-        
-      &:hover {
-        background-color: ${props => props.alt? 'salmon' : 'lightgreen'};
-        color: black;
-        }
-      `;
 
 class App extends Component {
   state = {
@@ -58,18 +44,6 @@ class App extends Component {
   };
 
   render () {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
 
     let persons = null;
 
@@ -101,9 +75,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>
+        <button className="button" alt={this.state.showPersons} onClick={this.togglePersonsHandler}>
           Toggle Persons
-        </StyledButton>
+        </button>
         {persons}
       </div>
       );

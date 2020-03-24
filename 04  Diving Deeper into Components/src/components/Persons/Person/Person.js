2f874c6bd4154234f68f6e10cfd842125a1ddd30
Toby Jorris <tobyjorris@gmail.com>
2020-03-24 09:37:54 -0500

    added ref to Person.js

--- 04  Diving Deeper into Components/src/components/Persons/Person/Person.js
+++ 04  Diving Deeper into Components/src/components/Persons/Person/Person.js
@@ -7,6 +7,16 @@ import withClass from "../../../hoc/withClass";
 
 
 class Person extends Component {
+
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log('[Person.js] rendering...');
         return (
@@ -15,8 +25,11 @@ class Person extends Component {
                 <p onClick={this.props.click}>
                     I'm {this.props.name} and I am {this.props.age} years old!
                 </p>
-                <p>{this.props.children}</p>
+                <p key="i2">{this.props.children}</p>
                 <input
+                    key="i3"
+                    //ref={(inputEl) => {this.inputElement = inputEl}}
+                    ref={this.inputElementRef}
                     type="text"
                     onChange={this.props.changed}
                     value={this.props.name}/>

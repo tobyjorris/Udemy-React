121e74a38762ffd4dd52c5c69ef468f09c38b54f
Toby Jorris <tobyjorris@gmail.com>
2020-03-24 09:22:29 -0500

    added prop-types dependency, using in Person.js

--- 04  Diving Deeper into Components/src/components/Persons/Person/Person.js
+++ 04  Diving Deeper into Components/src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment }from 'react';
+import PropTypes from 'prop-types';
 import classes from './Person.css'
 import Aux from '../../../hoc/Auxiliary.js';
 import withClass from "../../../hoc/withClass";
@@ -25,7 +26,12 @@ class Person extends Component {
     }
 }
 
-
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
 
 export default withClass(Person, classes.Person);

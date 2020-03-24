0077dd0644fd81cd8a9d94dc2200b18b922ea878
Toby Jorris <tobyjorris@gmail.com>
2020-03-24 12:51:28 -0500

    updated the Auth context to utilize useContext

--- 04  Diving Deeper into Components/src/components/Persons/Person/Person.js
+++ 04  Diving Deeper into Components/src/components/Persons/Person/Person.js
@@ -12,8 +12,11 @@ class Person extends Component {
         this.inputElementRef = React.createRef();
     }
 
+    static contextType = AuthContext;
+
     componentDidMount() {
         this.inputElementRef.current.focus();
+        console.log(this.context.authenticated);
     }
 
     render() {
@@ -22,7 +25,7 @@ class Person extends Component {
             //<Fragment>
             <Aux>
                 <AuthContext.Consumer>
-                    {(context) => context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
+                    {(context) => this.context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
                 </AuthContext.Consumer>
                 <p onClick={this.props.click}>
                     I'm {this.props.name} and I am {this.props.age} years old!

5abbb3dde05f1ecd9b6cefce1e2b850509ad3f5e
Toby Jorris <tobyjorris@gmail.com>
2020-03-26 08:22:35 -0500

    Hooked the modal & Order button up

--- 05 The Burger Builder (Basic Version)/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 05 The Burger Builder (Basic Version)/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,7 +23,12 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
+    };
+
+    purchaseHandler = () => {
+        this.setState({purchasing: true})
     };
 
     updatePurchaseState(ingredients) {
@@ -77,7 +82,7 @@ class BurgerBuilder extends Component {
         }
         return (
             <Aux>
-                <Modal>
+                <Modal show={this.state.purchasing}>
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
@@ -87,6 +92,7 @@ class BurgerBuilder extends Component {
                     disabled={disabledInfo}
                     purchasable={this.state.purchasable}
                     price={this.state.totalPrice}
+                    ordered={this.purchaseHandler}
                 />
 
             </Aux>

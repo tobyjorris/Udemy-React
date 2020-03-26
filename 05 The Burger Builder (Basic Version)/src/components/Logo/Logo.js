f620714f8dd73e1d7392c5be8b5001019f6827e3
Toby Jorris <tobyjorris@gmail.com>
2020-03-26 09:45:28 -0500

    Adjusted logo to change sizes between desktop & mobile

--- 05 The Burger Builder (Basic Version)/src/components/Logo/Logo.js
+++ 05 The Burger Builder (Basic Version)/src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import burgerLogo from "../../assets/images/original.png";
 import classes from './Logo.css'
 
 const logo = (props) => (
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height: props.height}}>
         <img src={burgerLogo} alt="MyBurger"/>
     </div>
 );

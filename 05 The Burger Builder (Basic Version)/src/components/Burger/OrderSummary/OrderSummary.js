63aebefe4aa0490521c538ff5fe545905f730d9f
Toby Jorris <tobyjorris@gmail.com>
2020-03-27 09:45:23 -0500

    Made some optimizations to Modal, Order Summary (lifecycle hooks)

--- 05 The Burger Builder (Basic Version)/src/components/Burger/OrderSummary/OrderSummary.js
+++ 05 The Burger Builder (Basic Version)/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../../hoc/Auxiliary';
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
-            return <li key={igKey}>
-                        <span style={{ textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-                    </li>
-        })
-    ;
+class orderSummary extends Component {
+    //this could be a functional component, doesn't have to be a class based component - did it just to see the lifecycle hooks
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: ${props.purchasePrice.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.cancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.checkout}>CONTINUE</Button>
-        </Aux>
-    )
+    componentDidUpdate(nextProps, nextState, nextContext) {
+        console.log('[OrderSummary] WillUpdate')
+    }
+
+    render() {
+
+        const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+                return <li key={igKey}>
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                </li>
+            });
+
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: ${this.props.purchasePrice.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.cancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.checkout}>CONTINUE</Button>
+            </Aux>
+        )
+    }
 };
 
 export default orderSummary;
+
--- 05 The Burger Builder (Basic Version)/src/components/Navigation/SideDrawer/ToggleButton/ToggleButton.js
+++ 05 The Burger Builder (Basic Version)/src/components/Navigation/SideDrawer/ToggleButton/ToggleButton.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Aux from '../../../../hoc/Auxiliary';
 import classes from './ToggleButton.css';
 
 const toggleBtn = (props) => (
